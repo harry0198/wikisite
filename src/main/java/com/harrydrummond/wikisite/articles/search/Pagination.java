8589b53b9f67fabb9cdf55a718c8d259df5fbf3a
harry0198 <Harry@drummail.co.uk>
2022-04-24 00:48:23 +0100

    Merge pull request #10
    * Added templating and fragment css/html for pagination
    
    * Added pagination to search page

---
+++ src/main/java/com/harrydrummond/wikisite/articles/search/Pagination.java
@@ -0,0 +1,24 @@
+package com.harrydrummond.wikisite.articles.search;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Pagination<T> {
+
+    private final List<T> content;
+    private final long totalHits;
+    private final int pageSize;
+
+    public int calcTotalPages() {
+        return (int) Math.ceil((double) totalHits / pageSize);
+    }
+
+
+}
